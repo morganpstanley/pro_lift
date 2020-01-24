43cc0490fe3616dae6e273eaec2a760fe2c477e1
morganpstanley <morganpstanley@gmail.com>
2020-01-24 16:10:14 -0700

    add each exercises lift assocations to index

--- frontend/index.js
+++ frontend/index.js
@@ -6,9 +6,17 @@ function run() {
     .then(resp => resp.json())
     .then(json => 
         json.forEach(exercise => {
-            let p = document.createElement('p');
-            p.innerText = exercise.name;
-            document.querySelector('body').appendChild(p); 
+            let div = document.createElement('div');
+            div.innerHTML = `<b> ${exercise.name} </b> `
+            div.classList.add('lift');
+            exercise.lifts.forEach(lift => {
+                let p = document.createElement('p')
+                liftString = lift.reps + " x " + lift.weight;
+                console.log(liftString)
+                p.innerHTML = liftString;
+                div.appendChild(p)
+            })
+            document.querySelector('body').appendChild(div); 
         })
     );
 }
