d79b9df831b640e7e9bec96d6e6b31278ec675fd
morganpstanley <morganpstanley@gmail.com>
2020-01-24 16:11:29 -0700

    add basic index, destroy, and create actions to LiftsController

--- backend/app/controllers/lifts_controller.rb
+++ backend/app/controllers/lifts_controller.rb
@@ -1,2 +1,16 @@
 class LiftsController < ApplicationController
+    def index
+        lifts = Lift.all
+        render json: lifts
+    end
+
+    def destroy
+        lift = Lift.find_by_id(params[:id])
+        Lift.destroy(params[:id])
+    end
+
+    def create
+        lift = Lift.create(reps: params[:reps], date: params[:amount])
+        render json: lift
+    end
 end
