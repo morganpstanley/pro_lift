98e79454f41cbe0d7e8e44b256eedec24ff379f4
morganpstanley <morganpstanley@gmail.com>
2020-01-23 23:15:59 -0700

    add basic index, destroy, and create actions

--- backend/app/controllers/exercises_controller.rb
+++ backend/app/controllers/exercises_controller.rb
@@ -1,2 +1,16 @@
 class ExercisesController < ApplicationController
+    def index
+        exercises = Exercise.all
+        render json: exercises
+    end
+
+    def destroy
+        exercise = Excercise.find_by_id(params[:id])
+        Exercise.destroy(params[:id])
+    end
+
+    def create
+        exercise = Exercise.create(name: params[:name], date: params[:date])
+        render json: exercise
+    end
 end
